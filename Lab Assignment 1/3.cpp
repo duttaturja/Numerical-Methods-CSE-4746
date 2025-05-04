9bf618e77bb52f18d6ace6b9ea17adacfff398e0
Turja Dutta <133532872+duttaturja@users.noreply.github.com>
2025-05-05 00:52:07 +0600

    Update 3.cpp

--- Lab Assignment 1/3.cpp
+++ Lab Assignment 1/3.cpp
@@ -1,10 +1,19 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-
-int main() {
-    double x = 5, ans;
-    ans = ((x * x * x) - (2 * x * x) + (5 * x) + 10);
-    cout << "f(" << x << ") = " << ans << endl;
-    return 0;
-}
+#include<bits/stdc++.h> 
+using namespace std; 
+int horners(vector<int> nums, int x) 
+{ 
+    int sz = nums.size(); 
+    int res = nums[0]; 
+    for(int i = 1; i < sz; i++) 
+    { 
+        res = res * x + nums[i]; 
+    }
+    return res; 
+} 
+int main() 
+{ 
+    //f(x) = x^3 - 2x^2 + 5x + 10 for x = 5 
+    vector<int> coefficients = {1, -2, 5, 10}; 
+    int x = 5; 
+    cout << "Value of polynomial using horner's is : " << horners(coefficients, x) << endl; 
+} 
