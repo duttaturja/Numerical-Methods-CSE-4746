214f73130018d89eabb7ac54ad060e5628bef13f
Turja Dutta <133532872+duttaturja@users.noreply.github.com>
2025-05-05 00:55:49 +0600

    Update 1.cpp

--- Lab Assignment 1/1.cpp
+++ Lab Assignment 1/1.cpp
@@ -1,16 +1,27 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-int main() {
-    long long num;
-    cout << "Enter a number: ";
-    cin >> num;
-    int count = 0;
-    while (num > 0) {
-        count++;
-        num /= 10;
-    }
-    cout << "Significant digits: " << count << endl;
-    return 0;
+#include<bits\stdc++.h> 
+using namespace std; 
+int main() 
+{ 
+    string num; 
+    cout << "Input numbers : " << endl; 
+    cin >> num; 
+    int significantDigits = 0; 
+    int zero = 0; 
+    for(int i = 0; i < num.size(); i++) 
+    { 
+        if (num[i] >= '1' && num[i] <= '9') 
+        { 
+            zero = 1; 
+            significantDigits++; 
+        } 
+        else if (num[i] == '0' && zero) 
+        { 
+            significantDigits++; 
+        } 
+        else if (num[i] == '.') 
+        { 
+            continue; 
+        } 
+    } 
+    cout << "Number of significant digits : " <<  significantDigits << endl; 
 }
