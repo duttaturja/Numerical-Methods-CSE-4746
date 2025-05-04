bf6da87c504bfad476c842249270c9b69c4819fc
Turja Dutta <133532872+duttaturja@users.noreply.github.com>
2025-05-05 00:58:46 +0600

    Update 2.cpp

--- Lab Assignment 1/2.cpp
+++ Lab Assignment 1/2.cpp
@@ -1,27 +1,41 @@
-#include<bits\stdc++.h> 
+#include<bits/stdc++.h> 
 using namespace std; 
+double round_off(double N, double n) 
+{ 
+	int h; 
+	double l, a, b, c, d, e, i, j, m, f, g; 
+	b = N; 
+	c = floor(N); 
+	for (i = 0; b >= 1; ++i) 
+		b = b / 10;
+		
+	d = n - i; 
+	b = N; 
+	b = b * pow(10, d); 
+	e = b + 0.5; 
+	
+	if ((float)e == (float)ceil(b))
+	{ 
+		f = (ceil(b)); 
+		h = f - 2; 
+		if (h % 2 != 0)
+		{ 
+			e = e - 1; 
+		} 
+	} 
+	j = floor(e); 
+	m = pow(10, d); 
+	j = j / m; 
+	return j; 
+} 
 int main() 
 { 
-    string num; 
-    cout << "Input numbers : " << endl; 
-    cin >> num; 
-    int significantDigits = 0; 
-    int zero = 0; 
-    for(int i = 0; i < num.size(); i++) 
-    { 
-        if (num[i] >= '1' && num[i] <= '9') 
-        { 
-            zero = 1; 
-            significantDigits++; 
-        } 
-        else if (num[i] == '0' && zero) 
-        { 
-            significantDigits++; 
-        } 
-        else if (num[i] == '.') 
-        { 
-            continue; 
-        } 
-    } 
-    cout << "Number of significant digits : " <<  significantDigits << endl; 
+	double num; 
+	cout << "Enter number : " << endl; 
+	cin >> num; 
+	int decimalCount; 
+	cout << "Enter decimal precision : " << endl; 
+	cin >> decimalCount; 
+	cout << "Rounded output : " << endl; 
+	cout << round_off(num, decimalCount) << endl; 
 }
